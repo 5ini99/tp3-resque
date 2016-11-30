e00bd3c94b75716c5adfe5b591ca9f370186fe5b
xiaobo.sun <xiaobosun@gridinfo.com.cn>
2016-11-30 10:26:22 +0800

    init

---
+++ src/Resque/Exception.php
@@ -0,0 +1,13 @@
+<?php
+namespace Resque;
+/**
+ * Resque exception.
+ *
+ * @package		Resque
+ * @author		Chris Boulton <chris@bigcommerce.com>
+ * @license		http://www.opensource.org/licenses/mit-license.php
+ */
+class Exception extends \Exception
+{
+}
+?>
---
+++ src/Resque/Job/DontPerform.php
@@ -0,0 +1,13 @@
+<?php
+namespace Resque\Job;
+/**
+ * Exception to be thrown if a job should not be performed/run.
+ *
+ * @package		Resque/Job
+ * @author		Chris Boulton <chris@bigcommerce.com>
+ * @license		http://www.opensource.org/licenses/mit-license.php
+ */
+class DontPerform extends \Exception
+{
+
+}
