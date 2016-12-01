6135064fe6c98728a34b61723d2f9d4aff0dd1a9
xiaobo.sun <xiaobosun@gridinfo.com.cn>
2016-12-01 13:26:57 +0800

    优化autoload

--- autoload.php
+++ autoload.php
@@ -1,14 +1,9 @@
 <?php
-
-function classLoader($class)
-{
+spl_autoload_register(function ($class) {
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $file = __DIR__ . '/src/' . $path . '.php';
-
     if (file_exists($file)) {
         require_once $file;
     }
-}
-spl_autoload_register('classLoader');
-
-require_once  __DIR__ . '/src/functions.php';
+});
+require_once __DIR__ . '/src/functions.php';
